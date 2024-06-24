a6d3175e32dc4b43230c5a18bc2c1c3c6b6ad35f
AKFrick <ak.frick@promatis.ru>
2024-06-24 14:37:56 +0500

    Создал проект приложения

---
+++ PNTZ.Mufta.Launcher/Program.cs
@@ -0,0 +1,19 @@
+﻿using PNTZ.Mufta.Launcher.GUI;
+using System.Windows;
+
+namespace PNTZ.Mufta.Launcher
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+
+            MainWindow mainWin = new MainWindow();
+
+            app.Run(mainWin);
+
+        }
+    }
+}
