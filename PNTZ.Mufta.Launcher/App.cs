ea23e44e90cc25922a3237a8bd973c9f2766b026
AKFrick <ak.frick@promatis.ru>
2024-07-22 13:36:13 +0500

    Вынес логику по конфигурированию датапоинтов в отдельный класс

--- PNTZ.Mufta.Launcher/App.cs
+++ PNTZ.Mufta.Launcher/App.cs
@@ -16,7 +16,7 @@ namespace PNTZ.Mufta.Launcher
     {
         public App()
         {
-            DataPointConfigurator dataPointConfigurator = null;
+            DpBuilder dataPointConfigurator = null;
             Init += (_, _) =>
             {
                 CliLogger logger = new CliLogger(_cli);
@@ -26,14 +26,14 @@ namespace PNTZ.Mufta.Launcher
 
                 Heartbeat heartbeat = new Heartbeat(_cli) { Name = "Heartbeat1" };
 
-                dataPointConfigurator = new DataPointConfigurator(logger, _currentDirectory + "/DpConfig.xml", 
+                dataPointConfigurator = new DpBuilder(logger, _currentDirectory + "/DpConfig.xml", 
                     new Type[] {typeof(OpcUaProvider)},
                     null,
-                    new IDpProcessor[] {recipeLoader, heartbeat }                    
+                    new IDpProcessor[] { recipeLoader, heartbeat }                    
                     );                             
 
-                _cli.RegisterCommand("print", (args) => (_cli as ICliProgram).WriteLine(args[0]));
 
+                _cli.RegisterCommand("print", (args) => (_cli as ICliProgram).WriteLine(args[0]));
                 
                 _cli.RegisterCommand("init", (_) => recipeLoader.DpInitialized());
                 _cli.RegisterCommand("startpr", async (_) => await Task.Run(() => dataPointConfigurator.StartProviders()));
@@ -46,10 +46,7 @@ namespace PNTZ.Mufta.Launcher
                 _mainWindow = new MainView(mainViewModel);
             };
 
-            BeforeExit += async (sender, args) =>
-            {
-                await Task.Run(() => dataPointConfigurator.StopProviders());
-            };
+            BeforeExit += async (sender, args) => await Task.Run(() => dataPointConfigurator?.StopProviders());
         }
     }
 }
