003472ca1bc6f886d6a74e8146f1e1f5e7e7a618
AKFrick <ak.frick@promatis.ru>
2024-07-22 16:18:56 +0500

    Добавил атрибут имя точки

--- PNTZ.Mufta.Domain/RecipeHandling/RecipeLoader.cs
+++ PNTZ.Mufta.Domain/RecipeHandling/RecipeLoader.cs
@@ -37,24 +37,40 @@ namespace PNTZ.Mufta.RecipeHandling
             
             _logger.Info($"Рецепт загружен");
         }
-
+        uint awaitedCommand = 0;
         public async void Load()
         {
             await Task.Run(() =>
             {
                 _logger.Info("Устанавливаем команду 10");
                 SetLoadCommand.Value = 10;
+                
                 _logger.Info("Ждем ответа");
-                CommandFeedback.ValueUpdated += (s, v) =>
-                {
-                    if (v == 20) commandAccepted.Set();
-                };
+                awaitedCommand = 20;
+                CommandFeedback.ValueUpdated += CommandFeedback_ValueUpdated;                
                 commandAccepted.WaitOne();
+                CommandFeedback.ValueUpdated -= CommandFeedback_ValueUpdated;
+
                 _logger.Info("Устанавливаем команду 20");
                 SetLoadCommand.Value = 20;
             });
             _logger.Info("Рецепт загружен!");
         }
+        
+        private void CommandFeedback_ValueUpdated(object? sender, uint e)
+        {
+            if (e == awaitedCommand)
+            {
+                commandAccepted.Set();
+                _logger.Info("Команда получена");
+            }
+            else
+            {
+                throw new Exception("Пришла неправильная команда!");
+            }
+
+        }
+
 
 
         #region DataPoints
