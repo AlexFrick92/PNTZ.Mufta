7f2a2a24f2294d0c909031c5e1f62e25da103352
AKFrick <ak.frick@promatis.ru>
2024-07-29 15:03:17 +0500

    try to line chart

--- PNTZ.Mufta.Launcher/View/Chart/TnTqChart.xaml.cs
+++ PNTZ.Mufta.Launcher/View/Chart/TnTqChart.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PNTZ.Mufta.Launcher.ViewModel.Chart;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,12 @@ namespace PNTZ.Mufta.Launcher.View.Chart
     /// <summary>
     /// Interaction logic for TnTqChart.xaml
     /// </summary>
-    public partial class TnTqChart : UserControl
+    public partial class TnTqChart : System.Windows.Controls.UserControl
     {
-        public TnTqChart()
+        public TnTqChart(ChartViewModel viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
         }
     }
 }
