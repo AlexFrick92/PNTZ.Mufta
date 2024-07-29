34f2f95d597a4daaf82adeed1dc2dba01f0a7cf3
AKFrick <ak.frick@promatis.ru>
2024-07-29 09:14:27 +0500

    downgrade to .net6

---
+++ PNTZ.Mufta.Launcher/View/Chart/TnTqChart.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PNTZ.Mufta.Launcher.View.Chart
+{
+    /// <summary>
+    /// Interaction logic for TnTqChart.xaml
+    /// </summary>
+    public partial class TnTqChart : UserControl
+    {
+        public TnTqChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
