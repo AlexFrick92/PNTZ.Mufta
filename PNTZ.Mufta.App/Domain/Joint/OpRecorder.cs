bf32f508175ebdac6d5a345107d3595d1c658ca8
AKFrick <ak.frick@promatis.ru>
2024-08-06 11:13:27 +0500

    add submodule

--- PNTZ.Mufta.App/Domain/Joint/OpRecorder.cs
+++ PNTZ.Mufta.App/Domain/Joint/OpRecorder.cs
@@ -1,4 +1,5 @@
-﻿using Promatis.DataPoint.Interface;
+﻿using Promatis.DataPoint.Configuration;
+using Promatis.DataPoint.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace PNTZ.Mufta.App.Domain.Joint
 {
-    public class OpRecorder : IDpProcessor
+    public class OpRecorder : DpProcessor
     {
         public string Name { get; set; } = "OpRecorder1";    
         public void OnDpInitialized()
