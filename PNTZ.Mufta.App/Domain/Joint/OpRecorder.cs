63566357a09b8dd0bde9bc0ff5425f259a8abffa
AKFrick <ak.frick@promatis.ru>
2024-08-07 16:31:38 +0500

    revert to ~03b3422

--- PNTZ.Mufta.App/Domain/Joint/OpRecorder.cs
+++ PNTZ.Mufta.App/Domain/Joint/OpRecorder.cs
@@ -1,5 +1,4 @@
-﻿using Promatis.DataPoint.Configuration;
-using Promatis.DataPoint.Interface;
+﻿using Promatis.DataPoint.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace PNTZ.Mufta.App.Domain.Joint
 {
-    public class OpRecorder : DpProcessor
+    public class OpRecorder : IDpProcessor
     {
         public string Name { get; set; } = "OpRecorder1";    
         public void OnDpInitialized()
