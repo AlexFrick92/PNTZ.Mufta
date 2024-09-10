1924775abe122bb17be922645941f6c281fb2b38
AKFrick <ak.frick@promatis.ru>
2024-09-10 13:35:26 +0500

    Переключение кнопок

---
+++ PNTZ.Mufta.App/Domain/Joint/JointMode.cs
@@ -0,0 +1,12 @@
+﻿namespace PNTZ.Mufta.App.Domain.Joint
+{
+    public enum JointMode
+    {
+        Torque, //Контроль момента
+        TorqueShoulder, //Контроль момента и заплечника
+        Length, //Контроль длины
+        TorqueLength, //Контроль длины и момента
+        Jval, //Контроль значения J
+        TorqueJVal //Контроль значения J и момента
+    }
+}
--- PNTZ.Mufta.App/ViewModel/CreateRecipeViewModel.cs
+++ PNTZ.Mufta.App/ViewModel/CreateRecipeViewModel.cs
@@ -1,14 +1,48 @@
 ﻿using Desktop.MVVM;
+using DevExpress.Xpf.Core.ConditionalFormatting;
+using PNTZ.Mufta.App.Domain.Joint;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PNTZ.Mufta.App.ViewModel
 {
     public class CreateRecipeViewModel : BaseViewModel
     {
+        public CreateRecipeViewModel()
+        {
+            SetModeCommand = new RelayCommand((mode) => SetMode((JointMode)mode));
+        }
         public int SomeParamValue { get; set; } = 5;
+
+        public ICommand SetModeCommand { get; set; }
+
+        void SetMode(JointMode newMode)
+        {
+            SelectedMode = newMode;
+
+            Console.WriteLine("Установлен новый режим:" + newMode.ToString());
+        }
+
+
+        JointMode _selectedMode;
+        public JointMode SelectedMode
+        {
+            get => _selectedMode;
+            set
+            {
+                if (_selectedMode != value)
+                {
+                    _selectedMode = value;
+                    OnPropertyChanged(nameof(SelectedMode));                    
+                }
+            }
+        }
+
+
+
     }
 }
