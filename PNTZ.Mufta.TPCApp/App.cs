b7bde34bbe916915e71837e05935a22b4b642a0f
AKFrick <ak.frick@promatis.ru>
2024-11-18 14:38:12 +0500

    создал новый проект для приложения АРМа. Рефакторинг

---
+++ PNTZ.Mufta.TPCApp/App.cs
@@ -0,0 +1,39 @@
+﻿using Desktop.Application;
+using DpConnect;
+using DpConnect.Connection;
+using DpConnect.OpcUa;
+
+using PNTZ.Mufta.TPCApp.View;
+
+using Promatis.Core;
+using Promatis.IoC.DryIoc;
+
+
+namespace PNTZ.Mufta.TPCApp
+{
+    internal class App : StagedApplication
+    {
+        protected override void BeforeInit()
+        {
+            IIoCContainer container = new DryIocContainer();
+
+            container.RegisterInstance(container);
+
+            container.Register<IOpcUaConnection, OpcUaConnection>();
+            container.Register<IDpConnectionManager, ContainerizedConnectionManager>();
+
+            
+
+        }
+
+        protected override void Init()
+        {
+            mainWindow = new MainView();
+        }
+
+        protected override void AfterInit() 
+        {
+            
+        }
+    }
+}
---
+++ PNTZ.Mufta.TPCApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNTZ.Mufta.TPCApp
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.Start();
+        }
+    }
+}
