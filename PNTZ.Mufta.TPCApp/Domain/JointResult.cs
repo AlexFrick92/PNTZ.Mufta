25bdf2f62d4b1d345f4265bf7fdaaaa927b89f28
AKFrick <ak.frick@promatis.ru>
2024-11-28 08:45:50 +0500

    доменный объект - результат свинчивания

---
+++ PNTZ.Mufta.TPCApp/Domain/JointResult.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace PNTZ.Mufta.TPCApp.Domain
+{
+    public class JointResult
+    {
+        public float FinalTorque { get; set; }
+        public float FinalLength { get; set; }
+        public float FinalJVal { get; set; }
+        public float FinalTurns { get; set; }
+        public float FinalShoulderTorque { get; set; }
+        public float FinalShoulderTurns { get; set; }
+        public uint ResultPLC { get; set; }
+        public uint ResultTotal { get; set; }
+        public DateTime StartTimeStamp { get; set; }
+        public DateTime FinishTimeStamp { get; set; }
+
+        public List<TqTnLenPoint> Series { get; private set; }
+    }
+}
---
+++ PNTZ.Mufta.TPCApp/Domain/TqTnLenPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNTZ.Mufta.TPCApp.Domain
+{
+    public class TqTnLenPoint
+    {
+        public float Torque { get; set; }
+        public float Length { get; set; }
+        public float Turns { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
