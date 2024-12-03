f913c0a8a4afa81b62fd3bbcff7abc89015541bb
AKFrick <ak.frick@promatis.ru>
2024-12-03 16:53:49 +0500

    сохранение и загрузка рецептов из sqlite

--- PNTZ.Mufta.TPCApp/Repository/RepositoryContext.cs
+++ PNTZ.Mufta.TPCApp/Repository/RepositoryContext.cs
@@ -1,12 +1,11 @@
 ﻿
-
-
+using Microsoft.Data.Sqlite;
 using PNTZ.Mufta.TPCApp.Domain;
 
 using Promatis.Core.Logging;
 
 using System.Collections.Generic;
-
+using System.IO;
 using static PNTZ.Mufta.TPCApp.App;
 
 namespace PNTZ.Mufta.TPCApp.Repository
@@ -15,28 +14,81 @@ namespace PNTZ.Mufta.TPCApp.Repository
     {
         ILogger logger;
 
-        string StoragePath = App.AppInstance.CurrentDirectory + "/Repository/processdata.db";
+        string StoragePath = App.AppInstance.CurrentDirectory + "/Repository";
+        string conString;
 
         public RepositoryContext(ILogger logger)
         {
             this.logger = logger;
+            conString = $"Data Source={StoragePath}/processdata.db;Mode=ReadWriteCreate";
 
-            //using (var connection = new SqliteConnection($"Data Source={StoragePath}"))
-            //{
-            //    connection.Open();
-            //}
+            if (!Directory.Exists(StoragePath))            
+                Directory.CreateDirectory(StoragePath);            
+
+            using (var connection = new SqliteConnection(conString))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand();
+
+                command.Connection = connection;
+                command.CommandText = "CREATE TABLE IF NOT EXISTS Recipes(Name TEXT)";
+
+                command.ExecuteNonQuery();
+            }
         }
 
 
         public void SaveRecipes(IEnumerable<JointRecipe> jointRecipes)
         {
-            logger.Info("Рецепты сохранены.");
+            string sqlExpressionBase = $"INSERT INTO Recipes (Name) VALUES ";
+
+            string sqlValues = "";
+
+            foreach (JointRecipe jointRecipe in jointRecipes)
+            {
+                sqlValues += $"('{jointRecipe.Name}'),";
+            }
+
+            sqlValues = sqlValues.Remove(sqlValues.Length -1) +  ";";
+
+            using (var connection = new SqliteConnection(conString))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = sqlExpressionBase + sqlValues;
+                command.ExecuteNonQuery();
+            }
+
+            logger.Info("Сохранено");
+
         }
         public IEnumerable<JointRecipe> LoadRecipes()
         {
+            List<JointRecipe> recipes = new List<JointRecipe> ();
             logger.Info("Загружаем рецепты...");
+
+            using (var connection = new SqliteConnection(conString))
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT Name FROM Recipes;";
+                SqliteCommand command = new SqliteCommand(selectQuery);
+                command.Connection = connection;    
+                command.CommandText = selectQuery;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        recipes.Add(new JointRecipe() { Name = reader.GetString(reader.GetOrdinal("Name")) });
+
+                    }                
+                }
+            }
+
             logger.Info("Рецепты загружены.");
-            return new List<JointRecipe>() { new JointRecipe() { Name = "HSP-123" }, new JointRecipe() { Name = "DPV-123-5423"} };
+            return recipes;
         }
     }
 }
