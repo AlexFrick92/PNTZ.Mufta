eb54df71fefb9e869e92684318c74918676b476b
AKFrick <ak.frick@promatis.ru>
2024-12-10 11:48:50 +0500

    Перенёс генерацию в класс

---
+++ PNTZ.Mufta.TPCApp/Repository/JointRecipeMapperHelper.cs
@@ -0,0 +1,121 @@
+﻿using PNTZ.Mufta.TPCApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNTZ.Mufta.TPCApp.Repository
+{
+    public static class JointRecipeMapperHelper
+    {
+        public static JointRecipeMapper FromJointRecipe(this JointRecipeMapper mapper, JointRecipe recipe)
+        {
+            return new JointRecipeMapper()
+            {
+                Name = recipe.Name,
+                HEAD_OPEN_PULSES = recipe.HEAD_OPEN_PULSES,
+                TURNS_BREAK = recipe.TURNS_BREAK,
+                PLC_PROG_NR = recipe.PLC_PROG_NR,
+                LOG_NO = recipe.LOG_NO,
+                Tq_UNIT = recipe.Tq_UNIT,
+                SelectedThreadType = (int)recipe.SelectedThreadType,                
+                Thread_step = recipe.Thread_step,
+                PIPE_TYPE = recipe.PIPE_TYPE,
+
+                Box_Moni_Time = recipe.Box_Moni_Time,
+                Box_Len_Min = recipe.Box_Len_Min,
+                Box_Len_Max = recipe.Box_Len_Max,
+
+                Pre_Moni_Time = recipe.Pre_Moni_Time,
+                Pre_Len_Max = recipe.Pre_Len_Max,
+                Pre_Len_Min = recipe.Pre_Len_Min,
+
+                MU_Moni_Time = recipe.MU_Moni_Time,
+                MU_Tq_Ref = recipe.MU_Tq_Ref,
+                MU_Tq_Save = recipe.MU_Tq_Save,
+
+                SelectedMode = (int)recipe.JointMode,
+
+                MU_TqSpeedRed_1 = recipe.MU_TqSpeedRed_1,
+                MU_TqSpeedRed_2 = recipe.MU_TqSpeedRed_2,
+                MU_Tq_Dump = recipe.MU_Tq_Dump,
+                MU_Tq_Max = recipe.MU_Tq_Max,
+                MU_Tq_Min = recipe.MU_Tq_Min,
+                MU_Tq_Opt = recipe.MU_Tq_Opt,
+
+                MU_TqShoulder_Min = recipe.MU_TqShoulder_Min,
+                MU_TqShoulder_Max = recipe.MU_TqShoulder_Max,
+
+                MU_Len_Speed_1 = recipe.MU_Len_Speed_1,
+                MU_Len_Speed_2 = recipe.MU_Len_Speed_2,
+                MU_Len_Dump = recipe.MU_Len_Dump,
+                MU_Len_Min = recipe.MU_Len_Min,
+                MU_Len_Max = recipe.MU_Len_Max,
+
+                MU_JVal_Speed_1 = recipe.MU_JVal_Speed_1,
+                MU_JVal_Speed_2 = recipe.MU_JVal_Speed_2,
+                MU_JVal_Dump = recipe.MU_JVal_Dump,
+                MU_JVal_Min = recipe.MU_JVal_Min,
+                MU_JVal_Max = recipe.MU_JVal_Max,
+
+                TimeStamp = recipe.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss"),
+            };            
+        }
+
+        public static JointRecipe ToJointRecipe(this JointRecipeMapper mapper)
+        {
+            return new JointRecipe()
+            {
+                Name = mapper.Name,
+                HEAD_OPEN_PULSES = (float)mapper.HEAD_OPEN_PULSES,
+                TURNS_BREAK = (float)mapper.TURNS_BREAK,
+                PLC_PROG_NR = (ushort)mapper.PLC_PROG_NR,
+                LOG_NO = (ushort)mapper.LOG_NO,
+                Tq_UNIT = (ushort)mapper.Tq_UNIT,
+                SelectedThreadType = (ThreadType)mapper.SelectedThreadType,
+                Thread_step = (float)mapper.Thread_step,
+                PIPE_TYPE = mapper.PIPE_TYPE,
+
+                Box_Moni_Time = (int)mapper.Box_Moni_Time,
+                Box_Len_Min = (float)mapper.Box_Len_Min,
+                Box_Len_Max = (float)mapper.Box_Len_Max,
+
+                Pre_Moni_Time = (int)mapper.Pre_Moni_Time,
+                Pre_Len_Max = (float)mapper.Pre_Len_Max,
+                Pre_Len_Min = (float)mapper.Pre_Len_Min,
+
+                MU_Moni_Time = (int)mapper.MU_Moni_Time,
+                MU_Tq_Ref = (float)mapper.MU_Tq_Ref,
+                MU_Tq_Save = (float)mapper.MU_Tq_Save,
+
+                JointMode = (JointMode)mapper.SelectedMode,
+
+                MU_TqSpeedRed_1 = (float)mapper.MU_TqSpeedRed_1,
+                MU_TqSpeedRed_2 = (float)mapper.MU_TqSpeedRed_2,
+                MU_Tq_Dump = (float)mapper.MU_Tq_Dump,
+                MU_Tq_Max = (float)mapper.MU_Tq_Max,
+                MU_Tq_Min = (float)mapper.MU_Tq_Min,
+                MU_Tq_Opt = (float)mapper.MU_Tq_Opt,
+
+                MU_TqShoulder_Min = (float)mapper.MU_TqShoulder_Min,
+                MU_TqShoulder_Max = (float)mapper.MU_TqShoulder_Max,
+
+                MU_Len_Speed_1 = (float)mapper.MU_Len_Speed_1,
+                MU_Len_Speed_2 = (float)mapper.MU_Len_Speed_2,
+                MU_Len_Dump = (float)mapper.MU_Len_Dump,
+                MU_Len_Min = (float)mapper.MU_Len_Min,
+                MU_Len_Max = (float)mapper.MU_Len_Max,
+
+                MU_JVal_Speed_1 = (float)mapper.MU_JVal_Speed_1,
+                MU_JVal_Speed_2 = (float)mapper.MU_JVal_Speed_2,
+                MU_JVal_Dump = (float)mapper.MU_JVal_Dump,
+                MU_JVal_Min = (float)mapper.MU_JVal_Min,
+                MU_JVal_Max = (float)mapper.MU_JVal_Max,
+
+                TimeStamp = DateTime.Parse(mapper.TimeStamp)
+            };
+        }
+    }
+}
--- PNTZ.Mufta.TPCApp/Repository/RepositoryContext.cs
+++ PNTZ.Mufta.TPCApp/Repository/RepositoryContext.cs
@@ -42,44 +42,10 @@ namespace PNTZ.Mufta.TPCApp.Repository
             using (var connection = new SqliteConnection(connectionString))
             {
                 connection.Open();
-                var createTableQuery = GenerateCreateTableSql<JointRecipeMapper>(tableName);
+                var createTableQuery = SqlQueriesGenerator.CreateTable<JointRecipeMapper>(tableName);
                 connection.Execute(createTableQuery);
             }
-        }
-        private string GenerateCreateTableSql<T>(string TableName)
-        {           
-            var properties = typeof(T).GetProperties(); 
-
-            var columns = properties.Select(p =>
-            {
-                // Определение типа для каждого свойства
-                var columnType = GetSQLiteColumnType(p.PropertyType);
-
-                return $"{p.Name} {columnType}";
-            });
-
-            var sql = $"CREATE TABLE IF NOT EXISTS {TableName} ({string.Join(", ", columns)});";
-
-            return sql;
-        }
-        private static string GetSQLiteColumnType(Type propertyType)
-        {
-            // Маппинг типов .NET в типы SQLite
-            if (propertyType == typeof(int) || propertyType == typeof(long) || propertyType == typeof(short))
-                return "INTEGER";
-            if (propertyType == typeof(bool))
-                return "INTEGER"; // SQLite не имеет отдельного типа для boolean, используем INTEGER
-            if (propertyType == typeof(string))
-                return "TEXT";
-            if (propertyType == typeof(DateTime) || propertyType == typeof(DateTimeOffset))
-                return "TEXT"; // В SQLite DateTime обычно сохраняется в текстовом формате (ISO 8601)
-            if (propertyType == typeof(float) || propertyType == typeof(double) || propertyType == typeof(decimal))
-                return "REAL";
-            if (propertyType == typeof(byte[]) || propertyType == typeof(Guid))
-                return "BLOB";
-
-            return "TEXT"; // По умолчанию используем TEXT
-        }
+        }       
 
 
         //Операции над рецептами
@@ -87,268 +53,61 @@ namespace PNTZ.Mufta.TPCApp.Repository
         public void SaveRecipe(JointRecipe rec)
         {
 
-            //if (loadedRecipes.FirstOrDefault(r => r.Name == rec.Name) != null)
-              //  UpdateRecipe(rec);
-            //else
+            if (loadedRecipes.FirstOrDefault(r => r.Name == rec.Name) != null)
+                UpdateRecipe(rec);
+            else
                 InsertRecipe(rec);            
-
         }
-        private void InsertRecipe(JointRecipe rec)
+        private void InsertRecipe(JointRecipe recipe)
         {
             using (var connection = new SqliteConnection(recipesConnectionString))
             {
                 connection.Open();
-                var insertQuery = @"
-                    INSERT INTO Recipes (
+                var insertQuery = SqlQueriesGenerator.Insert<JointRecipeMapper>(recipeTableName);                  
 
-                    Name,
-                    HEAD_OPEN_PULSES,
-                    TURNS_BREAK,
-                    PLC_PROG_NR,
-                    LOG_NO,
-                    Tq_UNIT,
-                    SelectedThreadType,
-                    Thread_step,
-                    PIPE_TYPE,
-
-                    Box_Moni_Time,
-                    Box_Len_Min,
-                    Box_Len_Max,
-
-                    Pre_Moni_Time,
-                    Pre_Len_Max,
-                    Pre_Len_Min,
-
-                    MU_Moni_Time,
-                    MU_Tq_Ref,
-                    MU_Tq_Save,
-
-                    SelectedMode,
-
-                    MU_TqSpeedRed_1,
-                    MU_TqSpeedRed_2,
-                    MU_Tq_Dump,
-                    MU_Tq_Max,
-                    MU_Tq_Min,
-                    MU_Tq_Opt,
-
-                    MU_TqShoulder_Min,
-                    MU_TqShoulder_Max,
-
-                    MU_Len_Speed_1,
-                    MU_Len_Speed_2,
-                    MU_Len_Dump,
-                    MU_Len_Min,
-                    MU_Len_Max,
-
-                    MU_JVal_Speed_1,
-                    MU_JVal_Speed_2,
-                    MU_JVal_Dump,
-                    MU_JVal_Min,
-                    MU_JVal_Max,
-
-                    TimeStamp
-                    )
-
-                    VALUES (
-                    @Name,
-                    @HEAD_OPEN_PULSES,
-                    @TURNS_BREAK,
-                    @PLC_PROG_NR,
-                    @LOG_NO,
-                    @Tq_UNIT,
-                    @SelectedThreadType,
-                    @Thread_step,
-                    @PIPE_TYPE,
-
-                    @Box_Moni_Time,
-                    @Box_Len_Min,
-                    @Box_Len_Max,
-
-                    @Pre_Moni_Time,
-                    @Pre_Len_Max,
-                    @Pre_Len_Min,
-
-                    @MU_Moni_Time,
-                    @MU_Tq_Ref,
-                    @MU_Tq_Save,
-
-                    @SelectedMode,
-
-                    @MU_TqSpeedRed_1,
-                    @MU_TqSpeedRed_2,
-                    @MU_Tq_Dump,
-                    @MU_Tq_Max,
-                    @MU_Tq_Min,
-                    @MU_Tq_Opt,
-
-                    @MU_TqShoulder_Min,
-                    @MU_TqShoulder_Max,
-
-                    @MU_Len_Speed_1,
-                    @MU_Len_Speed_2,
-                    @MU_Len_Dump,
-                    @MU_Len_Min,
-                    @MU_Len_Max,
-
-                    @MU_JVal_Speed_1,
-                    @MU_JVal_Speed_2,
-                    @MU_JVal_Dump,
-                    @MU_JVal_Min,
-                    @MU_JVal_Max,
-
-                    @TimeStamp
-
-                    )
-                ";
-
-                JointRecipeMapper recipe = new JointRecipeMapper()
-                {
-                    Name = rec.Name,
-                    HEAD_OPEN_PULSES = rec.HEAD_OPEN_PULSES,
-                    TURNS_BREAK = rec.TURNS_BREAK,
-                    PLC_PROG_NR = rec.PLC_PROG_NR,
-                    LOG_NO = rec.LOG_NO,
-                    Tq_UNIT = rec.Tq_UNIT,
-                    SelectedThreadType = (int)rec.SelectedThreadType,                
-                    Thread_step = rec.Thread_step,
-                    PIPE_TYPE = rec.PIPE_TYPE,
-
-                    Box_Moni_Time = rec.Box_Moni_Time,
-                    Box_Len_Min = rec.Box_Len_Min,
-                    Box_Len_Max = rec.Box_Len_Max,
-
-                    Pre_Moni_Time = rec.Pre_Moni_Time,
-                    Pre_Len_Max = rec.Pre_Len_Max,
-                    Pre_Len_Min = rec.Pre_Len_Min,
-
-                    MU_Moni_Time = rec.MU_Moni_Time,
-                    MU_Tq_Ref = rec.MU_Tq_Ref,
-                    MU_Tq_Save = rec.MU_Tq_Save,
-
-                    SelectedMode = (int)rec.JointMode,
-
-                    MU_TqSpeedRed_1 = rec.MU_TqSpeedRed_1,
-                    MU_TqSpeedRed_2 = rec.MU_TqSpeedRed_2,
-                    MU_Tq_Dump = rec.MU_Tq_Dump,
-                    MU_Tq_Max = rec.MU_Tq_Max,
-                    MU_Tq_Min = rec.MU_Tq_Min,
-                    MU_Tq_Opt = rec.MU_Tq_Opt,
-
-                    MU_TqShoulder_Min = rec.MU_TqShoulder_Min,
-                    MU_TqShoulder_Max = rec.MU_TqShoulder_Max,
-
-                    MU_Len_Speed_1 = rec.MU_Len_Speed_1,
-                    MU_Len_Speed_2 = rec.MU_Len_Speed_2,
-                    MU_Len_Dump = rec.MU_Len_Dump,
-                    MU_Len_Min = rec.MU_Len_Min,
-                    MU_Len_Max = rec.MU_Len_Max,
-
-                    MU_JVal_Speed_1 = rec.MU_JVal_Speed_1,
-                    MU_JVal_Speed_2 = rec.MU_JVal_Speed_2,
-                    MU_JVal_Dump = rec.MU_JVal_Dump,
-                    MU_JVal_Min = rec.MU_JVal_Min,
-                    MU_JVal_Max = rec.MU_JVal_Max,
-
-                    TimeStamp = rec.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss"),
-
-                };
-                connection.Execute(insertQuery, recipe);
+ 
+                var mapper = new JointRecipeMapper().FromJointRecipe(recipe);
+                connection.Execute(insertQuery,  mapper);
             };
         }        
-
-
-        private void UpdateRecipe(JointRecipe rec)
+        private void UpdateRecipe(JointRecipe recipe)
         {
             using (var connection = new SqliteConnection(recipesConnectionString))
             {
                 connection.Open();
-                var updateQuery = @"
-                    UPDATE Users SET
-                    Name = @Name
+                var updateQuery = SqlQueriesGenerator.Update<JointRecipeMapper>(recipeTableName, "WHERE Name = @Name");
+                Console.WriteLine(updateQuery);
+                var mapper = new JointRecipeMapper().FromJointRecipe(recipe);
 
-
-                ";
-                var recipe = new JointRecipeMapper();
-
-                connection.Execute(updateQuery, recipe);
+                connection.Execute(updateQuery, mapper);
             };
-        }
-        
+        }        
         public void RemoveRecipe(JointRecipe recipe)
         {
 
         }
-
         public IEnumerable<JointRecipe> LoadRecipes()
         {
             
             using (var connection = new SqliteConnection(recipesConnectionString))
             {
                 connection.Open();
-                var selectQuery = $"SELECT * FROM {recipeTableName}";
-                var recipesFromDb = connection.Query(selectQuery).ToList();
+                var selectQuery = SqlQueriesGenerator.SelectFrom<JointRecipeMapper>(recipeTableName);
+                var recipesFromDb = connection.Query<JointRecipeMapper>(selectQuery).ToList();
 
                 loadedRecipes.Clear();
 
-                foreach (var rec in recipesFromDb)
+                foreach (JointRecipeMapper rec in recipesFromDb)
                 {
                     loadedRecipes.Add(
-                        new JointRecipe()
-                        {
-                            Name = rec.Name,
-                            HEAD_OPEN_PULSES = (float)rec.HEAD_OPEN_PULSES,
-                            TURNS_BREAK = (float)rec.TURNS_BREAK,
-                            PLC_PROG_NR = (ushort)rec.PLC_PROG_NR,
-                            LOG_NO = (ushort)rec.LOG_NO,
-                            Tq_UNIT = (ushort)rec.Tq_UNIT,
-                            SelectedThreadType = (ThreadType)rec.SelectedThreadType,
-                            Thread_step = (float)rec.Thread_step,
-                            PIPE_TYPE = rec.PIPE_TYPE,
-
-                            Box_Moni_Time = (int)rec.Box_Moni_Time,
-                            Box_Len_Min = (float)rec.Box_Len_Min,
-                            Box_Len_Max = (float)rec.Box_Len_Max,
-
-                            Pre_Moni_Time = (int)rec.Pre_Moni_Time,
-                            Pre_Len_Max = (float)rec.Pre_Len_Max,
-                            Pre_Len_Min = (float)rec.Pre_Len_Min,
-
-                            MU_Moni_Time = (int)rec.MU_Moni_Time,
-                            MU_Tq_Ref = (float)rec.MU_Tq_Ref,
-                            MU_Tq_Save = (float)rec.MU_Tq_Save,
-
-                            JointMode = (JointMode)rec.SelectedMode,
-
-                            MU_TqSpeedRed_1 = (float)rec.MU_TqSpeedRed_1,
-                            MU_TqSpeedRed_2 = (float)rec.MU_TqSpeedRed_2,
-                            MU_Tq_Dump = (float)rec.MU_Tq_Dump,
-                            MU_Tq_Max = (float)rec.MU_Tq_Max,
-                            MU_Tq_Min = (float)rec.MU_Tq_Min,
-                            MU_Tq_Opt = (float)rec.MU_Tq_Opt,
-
-                            MU_TqShoulder_Min = (float)rec.MU_TqShoulder_Min,
-                            MU_TqShoulder_Max = (float)rec.MU_TqShoulder_Max,
-
-                            MU_Len_Speed_1 = (float)rec.MU_Len_Speed_1,
-                            MU_Len_Speed_2 = (float)rec.MU_Len_Speed_2,
-                            MU_Len_Dump = (float)rec.MU_Len_Dump,
-                            MU_Len_Min = (float)rec.MU_Len_Min,
-                            MU_Len_Max = (float)rec.MU_Len_Max,
-
-                            MU_JVal_Speed_1 = (float)rec.MU_JVal_Speed_1,
-                            MU_JVal_Speed_2 = (float)rec.MU_JVal_Speed_2,
-                            MU_JVal_Dump = (float)rec.MU_JVal_Dump,
-                            MU_JVal_Min = (float)rec.MU_JVal_Min,
-                            MU_JVal_Max = (float)rec.MU_JVal_Max,
-
-                            TimeStamp = DateTime.Parse(rec.TimeStamp())
-                        });                    
+                        rec.ToJointRecipe()
+                       );
                 }
             }
 
             return loadedRecipes;
         }
+
 
 
         //Операции над результатами
