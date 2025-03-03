6cd06de22e2ae52b61cb211e78f55ba3a485258d
AKFrick <ak.frick@promatis.ru>
2025-03-03 14:38:23 +0500

    Подавление сообщения о предупреждении

--- PNTZ.Mufta.TPCApp/DpConnect/MachineParamFromPlc.cs
+++ PNTZ.Mufta.TPCApp/DpConnect/MachineParamFromPlc.cs
@@ -66,7 +66,7 @@ namespace PNTZ.Mufta.TPCApp.DpConnect
                         logger.Info("Цикличное прослушивание параметров машин.");
                         if (DpTpcCommand.IsConnected)
                         {
-                            StartAwaitingForMpAsync();
+                            _ = StartAwaitingForMpAsync();
                         }
                         else
                         {                            
