788bd5b0b6362b5196aaf3b6de2c267af7a90c82
AKFrick <ak.frick@promatis.ru>
2025-12-09 21:12:59 +0500

    JointDataView

---
+++ PNTZ.Mufta.TPCApp/ViewModel/Joint/JointProcessDataViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Desktop.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNTZ.Mufta.TPCApp.ViewModel.Joint
+{
+    public class JointProcessDataViewModel : BaseViewModel
+    {
+    }
+}
