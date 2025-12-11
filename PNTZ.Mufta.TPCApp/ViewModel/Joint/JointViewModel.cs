b50a3926afbc528be194b936d89c79e691ef29d1
AKFrick <ak.frick@promatis.ru>
2025-12-11 10:23:43 +0500

    JointViewModel

---
+++ PNTZ.Mufta.TPCApp/ViewModel/Joint/JointViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Desktop.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNTZ.Mufta.TPCApp.ViewModel.Joint
+{
+    /// <summary>
+    /// Процесс свинчивания
+    /// </summary>
+    public class JointViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// Графики
+        /// </summary>
+        public JointProcessChartViewModel JointProcessChartViewModel { get; set; } = new JointProcessChartViewModel();
+        /// <summary>
+        /// Панель данных слева
+        /// </summary>
+        public JointProcessDataViewModel JointProcessDataViewModel { get; set; } = new JointProcessDataViewModel();
+
+        public JointViewModel()
+        {
+            
+        }        
+    }
+}
