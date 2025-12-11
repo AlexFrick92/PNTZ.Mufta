1cae2d7090ab4b5cab15ac410081945196a2b94b
AKFrick <ak.frick@promatis.ru>
2025-12-11 14:54:03 +0500

    Комментарии к событиям

--- PNTZ.Mufta.TPCApp/Domain/IJointProcessWorker.cs
+++ PNTZ.Mufta.TPCApp/Domain/IJointProcessWorker.cs
@@ -8,13 +8,34 @@ namespace PNTZ.Mufta.TPCApp.Domain
 {
     public interface IJointProcessWorker
     {
+        /// <summary>
+        /// Оценка оператора
+        /// </summary>
+        /// <param name="result">1-годная, 2-брак</param>
         void Evaluate(uint result);
-
+        /// <summary>
+        /// Труба появилась на позиции силовой навёртки
+        /// </summary>
         event EventHandler<JointResult> PipeAppear;
+        /// <summary>
+        /// Начата запись графиков
+        /// </summary>
         event EventHandler<EventArgs> RecordingBegun;
+        /// <summary>
+        /// Запись графиков остановлена
+        /// </summary>
         event EventHandler<JointResult> RecordingFinished;
+        /// <summary>
+        /// Ожидание оценки оператора
+        /// </summary>
         event EventHandler<JointResult> AwaitForEvaluation;
+        /// <summary>
+        /// Новая точка с данными
+        /// </summary>
         event EventHandler<TqTnLenPoint> NewTqTnLenPoint;        
+        /// <summary>
+        /// Свинчивание завершено
+        /// </summary>
         event EventHandler<JointResult> JointFinished;
 
         bool CyclicallyListen { get; set; }
--- PNTZ.Mufta.TPCApp/ViewModel/Joint/JointViewModel.cs
+++ PNTZ.Mufta.TPCApp/ViewModel/Joint/JointViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using Desktop.MVVM;
+using PNTZ.Mufta.TPCApp.Domain;
+using Promatis.Core.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Toolkit.IO;
 
 namespace PNTZ.Mufta.TPCApp.ViewModel.Joint
 {
@@ -11,7 +14,14 @@ namespace PNTZ.Mufta.TPCApp.ViewModel.Joint
     /// Процесс свинчивания
     /// </summary>
     public class JointViewModel : BaseViewModel
-    {
+    {                
+        public JointViewModel(IJointProcessWorker jointProcessWorker, IRecipeLoader recipeLoader, ILogger logger, ICliProgram cliProgram)
+        {
+            _jointProcessWorker = jointProcessWorker;
+            _recipeLoader = recipeLoader;
+            _logger = logger;
+            _cliProgram = cliProgram;
+        }
         /// <summary>
         /// Графики
         /// </summary>
@@ -20,10 +30,10 @@ namespace PNTZ.Mufta.TPCApp.ViewModel.Joint
         /// Панель данных слева
         /// </summary>
         public JointProcessDataViewModel JointProcessDataViewModel { get; set; } = new JointProcessDataViewModel();
-
-        public JointViewModel()
-        {
-            
-        }        
+        //Приватные поля
+        private IJointProcessWorker _jointProcessWorker;
+        private IRecipeLoader _recipeLoader;
+        private ILogger _logger;
+        private ICliProgram _cliProgram;
     }
 }
