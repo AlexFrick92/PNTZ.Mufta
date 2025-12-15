b2523d11b08eb455d04daf3a14a3bc7df7c4232d
AKFrick <ak.frick@promatis.ru>
2025-12-15 15:35:50 +0500

    Тест EditRecipeView

--- PNTZ.Mufta.TPCApp/ViewModel/Recipe/EditRecipeViewModel.cs
+++ PNTZ.Mufta.TPCApp/ViewModel/Recipe/EditRecipeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Desktop.MVVM;
+using PNTZ.Mufta.TPCApp.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,20 @@ namespace PNTZ.Mufta.TPCApp.ViewModel.Recipe
 {
     public class EditRecipeViewModel : BaseViewModel
     {
+        public EditRecipeViewModel()
+        {
+            
+        }
+        /// <summary>
+        /// Рецепт, который редактируется
+        /// </summary>
+        public JointRecipe EditingRecipe { get; private set; }
+
+        public void SetEditingRecipe(JointRecipe recipe)
+        {
+            EditingRecipe = recipe ?? throw new ArgumentNullException(nameof(recipe));
+            OnPropertyChanged(nameof(EditingRecipe));
+        }
+
     }
 }
