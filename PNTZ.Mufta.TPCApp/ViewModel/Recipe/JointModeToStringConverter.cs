3e014590cabe6b7c1c985ad5222d5a4dee04accb
AKFrick <ak.frick@promatis.ru>
2025-12-17 14:59:33 +0500

    Конвертер для режима чтобы отобразить на UI

---
+++ PNTZ.Mufta.TPCApp/ViewModel/Recipe/JointModeToStringConverter.cs
@@ -0,0 +1,50 @@
+using PNTZ.Mufta.TPCApp.Domain;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PNTZ.Mufta.TPCApp.ViewModel.Recipe
+{
+    /// <summary>
+    /// Converter для преобразования JointMode enum в читаемый текст
+    /// </summary>
+    public class JointModeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is JointMode jointMode)
+            {
+                switch (jointMode)
+                {
+                    case JointMode.Torque:
+                        return "💪 По моменту";
+
+                    case JointMode.TorqueShoulder:
+                        return "💪 По моменту с контролем заплечника";
+
+                    case JointMode.Length:
+                        return "📐 По длине";
+
+                    case JointMode.TorqueLength:
+                        return "💪📐 По длине с контролем момента";
+
+                    case JointMode.Jval:
+                        return "📏 По значению J";
+
+                    case JointMode.TorqueJVal:
+                        return "💪📏 По значению J с контролем момента";
+
+                    default:
+                        return "не выбран";
+                }
+            }
+
+            return "не выбран";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
